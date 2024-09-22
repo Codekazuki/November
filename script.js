0a21393f28e6774898b6ccbd75acd27f38758ea3
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-09-22 22:24:12 +0100

    sprint

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+const num = [2, 3, 4, 5];
+
+const doubleNumber = num.map((dn) => dn * 2);
+console.log(num);
+
+console.log(doubleNumber);
+const binaNum = num.map((bn) => bn.toString(2));
+console.log(binaNum);
+
+const two = document.querySelector(".two");
+two.style.color = "yellow";
+let hello;
+console.log(hello);
+hello = "greeting";
+console.log(hello);
+console.log(hey);
+let hey;
